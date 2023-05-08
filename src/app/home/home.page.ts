a4f723d8a2c366c5a1633a9fe04f7cc136ea1618
dtarnawsky <damian@ionic.io>
2023-05-08 15:27:19 -0700

    fix: use official release

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CapacitorSecurityProvider, SecurityProviderStatus } from '@capacitor-community/security-provider';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,21 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  status: string = 'Unknown';
+  constructor() { }
+
+  async ngOnInit() {
+    if (Capacitor.getPlatform() !== 'android') {
+      this.status = 'Try on an Android device';
+      return;
+    }
+      console.log('CapacitorSecurityProvider.installIfNeeded');
+      const result = await CapacitorSecurityProvider.installIfNeeded();
+      console.log(result);
+      this.status = result.status;
+      if (result.status !== SecurityProviderStatus.Success) {
+        alert('Play Services might be disabled / invalid.');
+      }
+  }
 }
